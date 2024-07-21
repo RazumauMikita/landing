2947a32bbd78416187a0f05aded4433b7fca8cae
Mikita <anyket2solo@gmail.com>
2024-07-21 20:36:14 +0300

    feat: add header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,24 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
+
     extend: {
-      colors: {},
+      colors: {
+        orangeLight: "hsla(17, 100%, 63%, 1)",
+        orangeDark: "hsla(38, 80%, 31%, 1)",
+        yellowMedium: "hsla(38, 91%, 55%, 1)",
+        grayLight: "hsla(240, 3%, 13%, 0.7)",
+        veryLightGray: "hsla(0, 0%, 98%, 1)",
+        grayBlack: "hsla(240, 3%, 13%, 1)",
+        blueLight: "hsla(176, 67%, 82%, 1)",
+        blueMedium: "hsla(173, 48%, 47%, 1)",
+        violMedium: "hsla(289, 33%, 53%, 1)",
+        blackLight: "hsla(0, 0%, 0%, 0.7)",
+        veryLightBlack: "hsla(0, 0%, 0%, 0.55)",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
   },
   plugins: [],
